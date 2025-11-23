e4557a7803b592deb976283639acce03b6e393b1
proarcade-110328 <anormalrobloxianqwerty@gmail.com>
2025-11-23 09:54:43 +0900

    Program fully developed.

--- firstgitmaybe.py
+++ firstgitmaybe.py
@@ -1,1 +1,50 @@
-x = input("Enter any number.")
+def xinput():
+    while True:
+        x = input("Enter any number: ")
+        try:
+            x = int(x)
+            return x
+        except ValueError:
+            print("Invalid input.")
+while True:
+    x = xinput()
+    lengths = []
+    for n in range(x):
+        size = float(input(f"Enter the size of Variable {n+1}: "))
+        lengths.append(size)
+    variables1 = []
+    for size in lengths:
+        variables1.append(size)
+    print(variables1)
+    print("Now enter the calculations among +, -, *, /, %, //, **.\n")
+    variables2 = []
+    for k in range(x-1):
+        calc = input(f"Enter calculation {k+1}: ")
+        variables2.append(calc)
+    print(variables2)
+    answer = variables1[0]
+    for i in range(len(variables2)):
+        operation = variables2[i]
+        nextnum = variables1[i+1]
+        if operation == "+":
+            answer += nextnum
+        elif operation == "-":
+            answer -= nextnum
+        elif operation == "*":
+            answer *= nextnum
+        elif operation == "/":
+            answer /= nextnum
+        elif operation == "%":
+            answer %= nextnum
+        elif operation == "//":
+            answer //= nextnum
+        elif operation == "**":
+            answer **= nextnum
+        else:
+            print(f"Invalid operation.")
+            break
+    print(f"{answer} is your result.")
+    again = input("Do you want to do another calculation? (y/n)").lower()
+    if again != "y":
+        print("This ends the calculation program.")
+        break
