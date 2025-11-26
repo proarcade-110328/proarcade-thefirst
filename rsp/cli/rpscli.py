1faacb23d1f1f3b0756892db8581bd583aaca544
proarcade-110328 <anormalrobloxianqwerty@gmail.com>
2025-11-26 19:49:37 +0900

    Retry.

--- rsp/cli/rpscli.py
+++ rsp/cli/rpscli.py
@@ -1,16 +1,25 @@
 import random
 cpu = ['rock', 'paper', 'scissors']
-computer = random.choice(cpu)
 print("Let's play a Rock, Paper Scissors.")
-usergame = input("Enter among rock, paper, scissors: ").lower()
-if usergame not in cpu:
-    print("Invalid input.")
-    quit()
-if (usergame == 'rock' and computer == 'scissors') or \
-    (usergame == 'paper' and computer == 'rock') or \
-    (usergame == 'scissors' and computer == 'paper'):
-    print("YOU'RE A WINNER!")
-elif usergame == computer:
-    print("NOBODY WINS.")
-else:
-    print("YOU LOSE!")
+while True:
+    usergame = input("Enter among rock, paper, scissors: ").lower()
+    if usergame in cpu:
+        print(f"You chose {usergame}.")
+        computer = random.choice(cpu)
+        if (usergame == 'rock' and computer == 'scissors') or \
+        (usergame == 'paper' and computer == 'rock') or \
+        (usergame == 'scissors' and computer == 'paper'):
+            print("YOU'RE A WINNER!")
+        elif usergame == computer:
+            print("NOBODY WINS.")
+        else:
+            print("YOU LOSE!")
+        again = input("Do you want to do another game? (y/n)").lower()
+        if again != "y":
+            print("This ends the game.")
+            break
+        else:
+            print("Starting a new game.\n")
+    else:
+        print("Invalid input.")
+        continue
