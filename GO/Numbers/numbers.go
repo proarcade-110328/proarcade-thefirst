ca4336683ecf30a942e0b501ecacecf074a1d967
proarcade-110328 <anormalrobloxianqwerty@gmail.com>
2025-11-30 13:26:05 +0900

    FizzBuzz.

---
+++ GO/Numbers/numbers.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 1
+	for i <= 30 {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
+		}
+		i += 1
+	}
+}
