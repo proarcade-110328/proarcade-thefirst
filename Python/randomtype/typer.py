e9b3e6e8e358b3089c37c5435406f2eb265264fd
proarcade-110328 <anormalrobloxianqwerty@gmail.com>
2025-12-14 12:17:00 +0900

    공백 오류 수정

--- Python/randomtype/typer.py
+++ Python/randomtype/typer.py
@@ -5,10 +5,9 @@ with open("ransentence.txt", "r", encoding = "UTF-8") as f:
 while True:
     ransen = random.choice(sentences)
     print (ransen)
-    time.sleep(3)
     start_perf = time.perf_counter()
     answer = input("아까 뜬 문장을 입력해주세요.")
-    if answer == ransen:
+    if answer.strip() == ransen.strip():
         end_perf = time.perf_counter()
         total_time = end_perf - start_perf
         print(f"타자 시간: {total_time}초.")
