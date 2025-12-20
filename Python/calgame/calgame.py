6a176a0ce79d832a1b471c462fd1b0ac6923e9fb
proarcade-110328 <anormalrobloxianqwerty@gmail.com>
2025-12-21 07:56:17 +0900

    Main program developed

---
+++ Python/calgame/calgame.py
@@ -0,0 +1,55 @@
+import random
+import time
+diffic = ['Easy', 'Normal', 'Hard']
+calclist = ['+', '-', '*', '/']
+
+def calc(fnum, snum, calcdif):
+    if calcdif == '+': return fnum + snum
+    if calcdif == '-': return fnum - snum
+    if calcdif == '*': return fnum * snum
+    return fnum / snum
+
+ranges = {
+    'Easy': 10,
+    'Normal': 100,
+    'Hard': 1000
+}
+
+while True:
+    print("Calculation Game.")
+    calcdif = input("What calculation do you want to do? ")
+    if calcdif in calclist:
+        print(f"Your calculation: {calcdif}.")
+    else:
+        print("Your calculation will be random.")
+        calcdif = random.choice(calclist)
+
+    gamedif = input("What difficulty do you want to do? ")
+    if gamedif in diffic:
+        print(f"Your difficulty: {gamedif}.")
+    else:
+        print("Your difficulty will be random.")
+        gamedif = random.choice(diffic)
+
+    start = time.perf_counter()
+
+    limit = ranges[gamedif]
+    fnum = random.randrange(1, limit + 1)
+    snum = random.randrange(1, limit + 1)
+    userans = float(input(f"{fnum} {calcdif} {snum} = "))
+    answer = calc(fnum, snum, calcdif)
+
+    if userans == answer:
+        end = time.perf_counter()
+        print("You're correct!")
+        totaltime = end - start
+        print(f"Calculation Time: {totaltime} Second(s)")
+    else:
+        print("You're NOT correct.")
+
+    again = input("Do you want to play again? (y/n) ").lower()
+    if again != "y":
+        print("This ends the calculation game.")
+        break
+    else:
+        print("Restarting...")
